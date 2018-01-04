702dba1b5d3dc33d5db4e2b47a282227126b9fc4
Alex Supkay <asupkay1124@gmail.com>
2018-01-04 16:14:20 -0500

    set up handlebars

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
+const static = express.static(__dirname +'/public');
+const bodyParser = require("body-parser");
+const configRoutes = require("./routes");
+
+app.use(bodyParser.json());
+
+const exphbs = require('express-handlebars');
+
+app.use("/public", static);
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+configRoutes(app);
+
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
